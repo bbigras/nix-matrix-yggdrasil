59557d52e2e0c16dbcc406796528810933c62807
Bruno Bigras <bigras.bruno@gmail.com>
2020-07-11 11:11:32 -0400

    use niv for dendrite too

--- default.nix
+++ default.nix
@@ -13,12 +13,7 @@ with pkgs;
 buildGoModule {
   name = "nix-matrix-yggdrasil";
 
-  src = fetchFromGitHub {
-    owner = "matrix-org";
-    repo = "dendrite";
-    rev = "5eeefdadf8e3881dd7a32559a92be49bd7ddaf47";
-    sha256 = "06x648xxzsi9g9jlb1r46m4qkj5lfa3v7kfz5z734kdgqgd9fll9";
-  };
+  src = fetchFromGitHub { inherit (sources.dendrite) owner repo rev sha256; };
 
   vendorSha256 = "1pvfrhmxbpkx8lbppah7xq0g52gxxg21pngcakj6d1zsam2yv775";
   subPackages = [ "cmd/dendrite-demo-yggdrasil" ];
