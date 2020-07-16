5438a3ab9f83c3bb246edc95b0402e869fa064a5
Bruno Bigras <bigras.bruno@gmail.com>
2020-07-16 09:51:30 -0400

    update the vendorSha256

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@ buildGoModule {
 
   src = fetchFromGitHub { inherit (sources.dendrite) owner repo rev sha256; };
 
-  vendorSha256 = "1pvfrhmxbpkx8lbppah7xq0g52gxxg21pngcakj6d1zsam2yv775";
+  vendorSha256 = "1ifzw957g7vil29lrxk062jng74b90s03wqn5h24xghz5vm4p9i8";
   subPackages = [ "cmd/dendrite-demo-yggdrasil" ];
 
   patches = [ ] ++ lib.optional riotX ./riotx.patch;
